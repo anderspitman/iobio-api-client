bdf8ed13d43ac2bf0c00a7cee23b696869f4207e
Anders Pitman <tapitman11@gmail.com>
2019-06-04 18:04:12 -0600

    Move piping logic to frontend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
 import { request } from 'xhr-stream-dl';
 
-console.log(request);
+
+export class Command {
+  constructor(name, args, options) {
+
+    this._server = "http://localhost:9001";
+
+    this._callbacks = {
+      'data': null,
+      'end': null,
+      'error': null,
+      'queue': null,
+      'exit': null,
+    };
+
+    this.pipeline = [
+      [name, args, options]
+    ];
+  }
+
+  pipe(name, args, options) {
+
+    const other = new Command(name, args, options);
+
+    this.pipeline = this.pipeline.concat(other.pipeline);
+
+    return this;
+  }
+
+  on(eventName, callback) {
+    if (this._callbacks[eventName] !== undefined) {
+      this._callbacks[eventName] = callback;
+    }
+    else {
+      const valid = Object.keys(this._callbacks).join(', ');
+      throw new Error(`Invalid event name "${eventName}". Valid options are [${valid}]`);
+    }
+  }
+
+  off(eventName) {
+    delete this._callbacks[eventName];
+  }
+
+  run() {
+    const pipeline = JSON.stringify(this.pipeline);
+    //const paramStr = encodeParams(params);
+    const query = encodeURI(this._server + "/call?pipeline=" + pipeline);
+    this._stream = request(query);
+
+    this._stream.on('data', this._callbacks['data']);
+    this._stream.on('end', this._callbacks['end']);
+    this._stream.on('error', this._callbacks['error']);
+    this._stream.on('queue', this._callbacks['queue']);
+    this._stream.on('exit', this._callbacks['exit']);
+
+    this._stream.run();
+  }
+}
+
 
 export class Api {
 
@@ -35,3 +92,7 @@ function encodeParams(obj) {
 
   return params.join('');
 }
+
+export const api = {
+  cmd: Command,
+};
