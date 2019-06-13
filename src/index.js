c41fd5614e0665001143e81ca24b6c29700e18c0
Anders Pitman <tapitman11@gmail.com>
2019-06-13 16:01:18 -0600

    Switch back to endpoint-based api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import { request } from 'xhr-stream-dl';
 
 
+class Api {
+
+  constructor(server) {
+    this.cmd = Command;
+    this._server = 'http://' + server;
+  }
+
+  alignmentHeader(url) {
+    return new Command(this._server, 'alignmentHeader', { url });
+  }
+
+  baiReadDepth(url) {
+    return new Command(this._server, 'baiReadDepth', { url });
+  }
+
+  craiReadDepth(url) {
+    return new Command(this._server, 'craiReadDepth', { url });
+  }
+
+  alignmentStatsStream(url, regions) {
+
+    //const regArr = regions.map(function(d) { return d.name+ ":"+ d.start + '-' + d.end;});
+    //const regStr = JSON.stringify(regions.map(function(d) { return {start:d.start,end:d.end,chr:d.name};}));
+    return new Command(this._server, 'alignmentStatsStream', { url, regions: JSON.stringify(regions) });
+  }
+}
+
+
 class Command {
-  constructor(name, args, server, options) {
+  constructor(server, endpoint, params) {
 
     this._server = server;
+    this._endpoint = endpoint;
+    this._params = params;
 
     this._callbacks = {
       'data': null,
@@ -13,19 +43,6 @@ class Command {
       'queue': null,
       'exit': null,
     };
-
-    this.pipeline = [
-      [name, args, options]
-    ];
-  }
-
-  pipe(name, args, options) {
-
-    const other = new Command(name, args, this._server, options);
-
-    this.pipeline = this.pipeline.concat(other.pipeline);
-
-    return this;
   }
 
   on(eventName, callback) {
@@ -43,9 +60,7 @@ class Command {
   }
 
   run() {
-    const pipeline = JSON.stringify(this.pipeline);
-    //const paramStr = encodeParams(params);
-    const query = encodeURI(this._server + "/call?pipeline=" + pipeline);
+    const query = encodeURI(this._server + '/' + this._endpoint + encodeParams(this._params));
     //console.log(query);
     this._stream = request(query);
 
@@ -56,44 +71,26 @@ class Command {
 }
 
 
-class Api {
+function encodeParams(obj) {
 
-  constructor(server) {
-    this.cmd = Command;
-    this._server = 'http://' + server;
-  }
+  const keys = Object.keys(obj);
 
-  call(name, args, options) {
-    return new Command(name, args, this._server, options);
-  }
+  const params = Object.keys(obj).map((key, i) => {
+    let sep = '&';
+    if (i === 0) {
+      sep = '?';
+    }
 
-  //call(endpoint, params) {
-  //  const paramStr = encodeParams(params);
-  //  const query = encodeURI(this._server + "/" + endpoint + paramStr);
-  //  const stream = request(query);
-  //  return stream;
-  //}
+    // TODO: might need this
+    //let value = encodeURIComponent(String(obj[key]));
+    const value = String(obj[key]);
+
+    return sep + key + '=' + value;
+  });
+
+  return params.join('');
 }
 
-//function encodeParams(obj) {
-//
-//  const keys = Object.keys(obj);
-//
-//  const params = Object.keys(obj).map((key, i) => {
-//    let sep = '&';
-//    if (i === 0) {
-//      sep = '?';
-//    }
-//
-//    // TODO: might need this
-//    //let value = encodeURIComponent(String(obj[key]));
-//    const value = String(obj[key]);
-//
-//    return sep + key + '=' + value;
-//  });
-//
-//  return params.join('');
-//}
 
 export {
   Api,
